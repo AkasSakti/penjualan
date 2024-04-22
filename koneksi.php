3477a8e0f6d71b3e7d216a5ec516dd41ac6a1137
Akas Bagus Setiawan <146935384+AkasSakti@users.noreply.github.com>
2024-04-22 10:03:38 +0700

    Add files via upload

--- koneksi.php
+++ koneksi.php
@@ -3,8 +3,9 @@ $servername	= "localhost";	// host atau IP
 $username	= "root";		// nama penggua
 $password	= "";			// password login
 $database	= "sdhpjl";		// database yang digunakan ketika melakukan query
+$port = "3308";
 // buat koneksi
-$conn = mysqli_connect($servername, $username, $password, $database);
+$conn = mysqli_connect($servername, $username, $password, $database, $port);
 // cek koneksi
 if (!$conn) {
     die("Connection failed: " . mysqli_error());
